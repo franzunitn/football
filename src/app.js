e2e75c9ccdc2a221e6f273eccc8b33184bc134e7
sfranchini <simone.franchini.tn@gmail.com>
2021-11-14 12:36:09 +0100

    sistemiamo endpoints +  status code

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
   var msg = {
     message : "Hello World, from football api. I'm working!"
   };  
+  res.statusCode = 200;
   res.json(msg);    
 });
 
@@ -20,24 +21,28 @@ app.get('/', (req, res) => {
 //get all players 
 app.get('/players', (req, res) => {
   var players = repo.getPlayers();
+  res.statusCode = 200;
   res.json(players);
 });
 
-//get a player with the ID
+//get a player by id
 app.get('/players/:playerId', (req, res) => {
   const player_id = parseInt(req.params.playerId);
   var player = repo.getPlayersId(player_id);
   if (player == null) {
     var msg = {
-      message : "Player not found!"
+      status: 404,
+      message : `Player with id ${player_id} not found!`
     };  
+    res.statusCode = 404;
     res.json(msg);
   } else {
+    res.statusCode = 200;
     res.json(player);
   }
 });
 
-//get all the players giving the role 
+//get players by role 
 app.get('/players/role/:role', (req, res) => {
   const role = req.params.role;
   var players = repo.getPlayers();
@@ -49,10 +54,13 @@ app.get('/players/role/:role', (req, res) => {
   }  
   if (response.length == 0) {
     var msg = {
+      status: 404,
       message : `There are no players with role '${role}'. Check the syntax correctness of the role specified`
     };  
+    res.statusCode = 404;
     res.json(msg);
   } else {
+    res.statusCode = 200;
     res.json(response);
   }
 });
@@ -62,8 +70,9 @@ app.get('/players/role/:role', (req, res) => {
 //get all teams
 app.get('/teams', (req, res) => {
   var teams = repo.getFantasyTeams();
+  res.statusCode = 200;
   if (teams.length == 0) {
-    var msg = {
+    var msg = {      
       message : "There are no teams. Create your own first!"
     };    
     res.json(msg);
@@ -72,16 +81,19 @@ app.get('/teams', (req, res) => {
     res.json(teams);
 });
 
-//get a team given the name
+//get team by id
 app.get('/teams/:teamId', (req, res) => {
   var id = req.params.teamId;
   var team = repo.getFantasyTeamsId(id);
   if (team != null){
+    res.statusCode = 200;
     res.json(team);
   } else {
     var msg = {
-      message : "Team not found!"
+      status: 404,
+      message : `Team with id '${id}' not found!`
     };  
+    res.statusCode = 404;
     res.json(msg);
   }  
 });
@@ -100,11 +112,14 @@ app.get('/teams/:teamId/gol', (req, res) => {
     var gols = {
       gols : gols
     }
+    res.statusCode = 200;
     res.json(gols);
   } else {
     var msg = {
-      message : "Team not found!"
+      status: 404,
+      message : `Team with id '${id}' not found!`
     };  
+    res.statusCode = 404;
     res.json(msg);
   }  
 });
@@ -112,26 +127,31 @@ app.get('/teams/:teamId/gol', (req, res) => {
 //create your team given the name of the team
 app.post('/teams', (req, res) => {
   var team = repo.createFantasyTeam(req.body.name);
+  res.statusCode = 200;
   res.json(team);  
 });
 
 //add a player to a team 
-app.put('/teams/:teamId', (req, res) => {
+app.put('/teams', (req, res) => {
   const playerId = parseInt(req.body.playerId);
-  const teamId = req.params.teamId;
+  const teamId = parseInt(req.body.teamId);
 
   if (repo.getPlayersId(playerId) == null) {
     var msg = {
+      status: 404,
       message : `There are no player with id ${playerId}`
     };  
+    res.statusCode = 404;
     res.json(msg);
   } else if (repo.getFantasyTeamsId(teamId) == null) {
     var msg = {
+      status: 404,
       message : `There are no team with id '${teamId}'`
     };  
     res.json(msg);
   } else {
-    var team = repo.insertPlayer(req.params.teamId, playerId);
+    var team = repo.insertPlayer(teamId, playerId);
+    res.statusCode = 200;
     res.json(team);  
   }
 });
@@ -142,28 +162,43 @@ app.delete('/teams/:teamId', (req, res) => {
   var team = repo.deleteFantasyTeam(teamId);
   if (team == null) {
     var msg = {
+      status: 404,
       message : `No team '${teamId}' found to delete in your fantasyTeams`
     };  
+    res.statusCode = 404;
     res.json(msg);
   } else {
+    res.statusCode = 200;
     res.json(team);
   }  
 });
 
-//delete a player from a team 
+//delete a player from team 
 app.delete('/teams/:teamId/player/:playerId', (req, res) => {
   const teamId = req.params.teamId;
   const playerId = parseInt(req.params.playerId);
   var player = repo.removePlayer(teamId, playerId);
   if (player == null) {
     var msg = {
+      status: 404,
       message : `No playerId '${playerId}' found to delete in team '${teamId}'`
     };  
+    res.statusCode = 404;
     res.json(msg);
   } else {
+    res.statusCode = 200;
     res.json(player);
   }  
 });
+
+//invalid route handling (Must be as last call endpoint!)
+app.all('*', function (req, res) {
+  var msg = {
+    message : "Invalide route!"
+  };  
+  res.statusCode = 400;
+  res.json(msg); 
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
